c2d935f2f31f0fb4c8bace8c8dbfd59dda3ec45f
sun33sun <1255317579@qq.com>
2024-05-28 16:02:58 +0800

    no message

---
+++ Assets/QuestionKit/Editor/QuestionEditorWindow.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using ProjectBase.Exam;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Serialization;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace QuestionKit
+{
+	public class QuestionEditorWindow : OdinEditorWindow
+	{
+		[MenuItem("Tools/QuestionKit/QuestionEditWindow")]
+		private static void OpenWindow()
+		{
+			var window = GetWindow<QuestionEditorWindow>();
+
+			window.position = GUIHelper.GetEditorWindowRect().AlignCenter(700, 700);
+		}
+
+		[OdinSerialize] private string textAssetName;
+		[OdinSerialize] private List<QuestionData> questionDatas;
+
+		private string GetPath(string fileName) => $"{Application.dataPath}/QuestionKit/Resources/{fileName}.json";
+
+		[PropertySpace(20)][Button]
+		private void LoadJson(TextAsset newTextAsset)
+		{
+			textAssetName = newTextAsset.name;
+			string path = GetPath(textAssetName);
+			string content = File.ReadAllText(path);
+			questionDatas = JsonConvert.DeserializeObject<List<QuestionData>>(newTextAsset.text);
+		}
+
+		[PropertySpace(20)]
+		[Button]
+		private void SaveJson()
+		{
+			if (textAssetName.IsNullOrWhitespace())
+				return;
+
+			string content = JsonConvert.SerializeObject(questionDatas);
+			File.WriteAllText(GetPath(textAssetName), content);
+			AssetDatabase.Refresh();
+		}
+
+		[PropertySpace(20)]
+		[Button]
+		private void CheckQuestionData()
+		{
+
+		}
+	}
+}
---
+++ Assets/Scripts/UI/UIScrollView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIScrollView : ScrollRect, IPointerDownHandler, IPointerUpHandler
+{
+	public enum E_Player
+	{
+		None = 0,
+		First = 1 << 1,
+		Second = 1 << 2,
+		Third = 1 << 3,
+	}
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		print(1 << 1);
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+	}
+}
